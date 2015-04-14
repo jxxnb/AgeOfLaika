982f4f891b509df5d4fa8512503cc225e0504646
jxxnb <jbruno404@gmail.com>
2015-04-14 16:36:50 -0400

    initial commit - first rev

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var humanYearsTextField: UITextField!
+    @IBOutlet weak var dogYearsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertHumanToDogPressed(sender: UIButton) {
+        var humanYears = humanYearsTextField.text.toInt()!
+        
+        dogYearsLabel.text = "Dog Years: \(humanYears * 7)"
+        
+        humanYearsTextField.resignFirstResponder()
+    }
 
 }
 
